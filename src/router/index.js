8889fee3b78e8021c9d50fbcfc10871dd03c296f
Richard Estes <richard.estes48@gmail.com>
2020-10-15 14:07:10 -0700

    add landing page redirect

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import GetStarted from '../views/GetStarted.vue'
+import LandingPage from '../views/LandingPage.vue'
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
+  {
+    path: '/landingpage',
+    name:'LandingPage',
+    component: LandingPage
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class = "landing-page-main">
+        <p> Hello World </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LandingPage"
+}
+</script>
+
+<style scoped>
+.landing-page-main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
