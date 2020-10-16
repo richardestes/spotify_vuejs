579b5426235c9db27c7052d2695cbeee99a5f746
Richard Estes <richard.estes48@gmail.com>
2020-10-16 12:41:49 -0700

    fuck

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,27 +27,21 @@ export default {
   methods: {
     requestAuthorization() {
       var clientId = '40babe1ec57c4ce8a664f8304f0da622';
-      var authEndpoint = 'https://accounts.spotify.com/authorize';
       var state = this.generateState();
-      var responseType = 'code';
+      var spotifyEndpoint = 'https://accounts.spotify.com/authorize';
+      var responseType = 'token';
       var redirectUri = 'https://spotifystats.io/';
-      var scopes = 'user-read-private playlist-read-private user-library-read user-top-read user-read-recently-played';
-      var requestUrl = authEndpoint + '?client_id=' + clientId + '&redirect_uri=' + redirectUri + '&response_type=' + responseType + '&scope=' + scopes + '&state=' + state;
-      console.log(requestUrl);
-      var config = {
-        headers: {'Access-Control-Allow-Origin': '*'}
-      };
-      axios.get(requestUrl,config)
-        .then(res=>console.log(res))
-        .catch(err=>console.log(err))
-      
+      var scopes = 'user-read-private playlist-read-private user-library-read user-top-read user-read-recently-played'
+      var requestUrl = spotifyEndpoint + '?client_id=' + clientId + '&response_type=' + responseType + '&redirect_uri=' + redirectUri + '&scope=' + scopes + '&state=' + state;
+      window.location = requestUrl;
+      return;
     },
     redirectToSpotify() {
       var clientId = '40babe1ec57c4ce8a664f8304f0da622';
       var state = this.generateState();
       var spotifyEndpoint = 'https://accounts.spotify.com/authorize';
       var responseType = 'token';
-      var redirectUri = 'https://friendify.onrender.com/landingpage';
+      var redirectUri = 'https://spotifystats.io/';
       var scopes = 'user-read-private playlist-read-private user-library-read user-top-read user-read-recently-played'
       var requestUrl = spotifyEndpoint + '?client_id=' + clientId + '&response_type=' + responseType + '&redirect_uri=' + redirectUri + '&scope=' + scopes + '&state=' + state;
       window.location = requestUrl;
