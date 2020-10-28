cf6a71fded10e84793c9b48bd414aa0bade54243
Richard Estes <richard.estes48@gmail.com>
2020-10-28 15:48:38 -0700

    updated auth, gonna finish this mf project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
     </p>
     <img src="../assets/21008-social-media-concept.gif" class="landing-image" />
     <button
-      v-on:click="requestAuthorization"
+      v-on:click="redirectToSpotify"
       id="btn_get_started"
       class="btn btn-landing"
     >
@@ -25,7 +25,7 @@ export default {
     msg: String,
   },
   methods: {
-    requestAuthorization() {
+    redirectToSpotify() {
       var clientId = '40babe1ec57c4ce8a664f8304f0da622';
       var state = this.generateState();
       var spotifyEndpoint = 'https://accounts.spotify.com/authorize';
@@ -34,17 +34,6 @@ export default {
       var scopes = 'user-read-private playlist-read-private user-library-read user-top-read user-read-recently-played'
       var requestUrl = spotifyEndpoint + '?client_id=' + clientId + '&response_type=' + responseType + '&redirect_uri=' + redirectUri + '&scope=' + scopes + '&state=' + state;
       window.location = requestUrl;
-      return;
-    },
-    redirectToSpotify() {
-      // var clientId = '40babe1ec57c4ce8a664f8304f0da622';
-      // var state = this.generateState();
-      // var spotifyEndpoint = 'https://accounts.spotify.com/authorize';
-      // var responseType = 'token';
-      // var redirectUri = 'https://spotifystats.io/';
-      // var scopes = 'user-read-private playlist-read-private user-library-read user-top-read user-read-recently-played'
-      // var requestUrl = spotifyEndpoint + '?client_id=' + clientId + '&response_type=' + responseType + '&redirect_uri=' + redirectUri + '&scope=' + scopes + '&state=' + state;
-      // window.location = requestUrl;
       return;
     },
     generateState() {
