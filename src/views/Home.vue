06acea34ac140b8dbffd02a6e8dabcb53f5176c1
Richard Estes <richard.estes48@gmail.com>
2020-11-16 14:41:13 -0800

    added component god i'm stupid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,14 @@
 </template>
 
 <script>
+import Header from "/components/Header"
 export default {
   name: "Home",
   props: {
     msg: String,
+  },
+  components : {
+    Header
   },
   methods: {
     redirectToSpotify() {
