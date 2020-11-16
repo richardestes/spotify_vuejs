0b5c93d0e625053c03d84752d32c8f9e4f4f96a0
Richard Estes <richard.estes48@gmail.com>
2020-11-16 14:55:47 -0800

    updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// import Social from '../views/Social.vue'
+// import Stats from '../views/Stats.vue'
+// import About from '../views/About.vue'
 
 Vue.use(VueRouter)
 
