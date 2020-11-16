2e3b5487d66c2a01c52a31ad6e8e5ecdf86b0159
Richard Estes <richard.estes48@gmail.com>
2020-11-16 15:18:13 -0800

    updated divs

--- src/views/components/Header.vue
+++ src/views/components/Header.vue
@@ -1,9 +1,13 @@
 <template>
 <div class="header">
-  <router-link to="/">SpotifyStats.io</router-link>
-  <router-link to="/stats">Stats</router-link>
-  <router-link to="/social">Social</router-link>
-  <router-link to="/about">About</router-link>
+  <div class="d-flex justify-content-start">
+      <router-link to="/">SpotifyStats.io</router-link>
+  </div>
+  <div class="d-flex justify-content-end">
+      <router-link to="/stats">Stats</router-link>
+    <router-link to="/social">Social</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
 </div>
 </template>
 
