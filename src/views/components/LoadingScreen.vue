f500036089e56a3f081955c3a2f6c16e162aad76
Richard Estes <richard.estes48@gmail.com>
2020-12-14 17:47:12 -0800

    oops forgot to add the actual loading screen...

---
+++ src/views/components/LoadingScreen.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id = "main">
+    <img v-bind:src="require('../../assets/loading.gif')" class="loadingGif" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.main {
+  display: flex;
+  flex-direction: column;
+  max-width: 100%;
+  justify-content: center;
+  align-content: center;
+  background-color: #FFFFF9;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+.loadingGif {
+  width: 15vw;
+  align-self: center;
+  background-color: #FFFFF9;
+}
+</style>
