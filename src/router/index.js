41d7172689aff35b1e33d1b3735821603bc61a2a
Richard Estes <richard.estes48@gmail.com>
2021-03-18 12:07:22 -0700

    removed unnecessary pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Compare from '../views/Compare.vue'
-import Stats from '../views/Stats.vue'
-import About from '../views/About.vue'
-import Register from '../views/Register.vue'
-import PageNotFound from '../views/components/PageNotFound.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Stats from "../views/Stats.vue";
+import Register from "../views/Register.vue";
+import PageNotFound from "../views/components/PageNotFound.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/stats',
-    name: 'Stats',
+    path: "/stats",
+    name: "Stats",
     component: Stats,
   },
   {
-    path: '/compare',
-    name: 'Compare',
-    component: Compare
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
   {
-    path: '/about',
-    name: 'About',
-    component: About
+    path: "/*",
+    component: PageNotFound,
   },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
-  },
-  {
-    path: '/*',
-    component: PageNotFound
-  },
-]
+];
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
-})
+  mode: "history",
+});
 
-export default router
+export default router;
--- src/views/components/Header.vue
+++ src/views/components/Header.vue
@@ -1,21 +1,19 @@
 <template>
-<div class="header">
-  <div class="header-logo-container">
-      <router-link to="/" class="header-logo">SpotifyStats.io</router-link>
+  <div class="header">
+    <div class="header-logo-container">
+      <router-link to="/" class="header-logo">SpotifyStats.io PoC</router-link>
+    </div>
+    <div class="header-links">
+      <router-link to="/stats" class="header-link">Stats</router-link>
+      <router-link to="/register" class="header-link">Register</router-link>
+    </div>
   </div>
-  <div class="header-links">
-    <router-link to="/stats" class="header-link">Stats</router-link>
-    <router-link to="/compare" class="header-link">Compare</router-link>
-    <router-link to="/about" class="header-link">About</router-link>
-    <router-link to="/register" class="header-link">Register</router-link> 
-  </div>
-</div>
 </template>
 
 <script>
 export default {
-  name : "Header",
-}
+  name: "Header",
+};
 </script>
 
 <style scoped>
@@ -33,7 +31,7 @@ export default {
 }
 .header-logo {
   text-decoration: none;
-  color: #0E0E0E;
+  color: #0e0e0e;
   font-weight: bold;
 }
 .header-links {
@@ -41,14 +39,14 @@ export default {
   justify-content: flex-end;
   width: 50vw;
 }
-.header-links a{
+.header-links a {
   padding-left: 15px;
   padding-right: 15px;
   text-decoration: none;
-  color: #0E0E0E;
+  color: #0e0e0e;
   font-weight: bold;
 }
 .header-links a:hover {
-  color:  #FF3057;
+  color: #ff3057;
 }
 </style>
